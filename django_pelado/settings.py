fc5f4aac587b0bb797c5cd08286459ade37b0fce
nicky-eng <nicolasoyarzabal@gmail.com>
2023-01-27 17:08:32 +0100

    Ray Tahles method

--- django_pelado/settings.py
+++ django_pelado/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os.path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = "django-insecure-0!z7!_((h(n24gw06aq_xnc_*e)t3b_lwavzqjy0k8rzxeo3rh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-pelado.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
@@ -117,3 +118,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
--- django_pelado/wsgi.py
+++ django_pelado/wsgi.py
@@ -15,3 +15,4 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_pelado.settings")
 
 application = get_wsgi_application()
 
+app = application
